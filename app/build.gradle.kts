019e76acfc3fa205ec8ee883eed3306df08c86aa
milanstevic1992ts-gif <milanstevic1992ts@gmail.com>
2026-09-25 09:25:36 +0200

    release: bump Garanzia to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.milanstevic.garanzia"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
