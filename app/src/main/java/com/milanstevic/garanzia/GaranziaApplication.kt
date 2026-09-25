876aae7c04f0351d20d8138e63a40f875362af8a
milanstevic1992ts-gif <milanstevic1992ts@gmail.com>
2026-09-25 13:05:02 +0200

    fix(ocr): initialize OpenCV at app startup

--- app/src/main/java/com/milanstevic/garanzia/GaranziaApplication.kt
+++ app/src/main/java/com/milanstevic/garanzia/GaranziaApplication.kt
@@ -1,7 +1,15 @@
 package com.milanstevic.garanzia
 
 import android.app.Application
+import android.util.Log
+import com.paddle.ocr.util.OpenCVUtils
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GaranziaApplication : Application()
+class GaranziaApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val loaded = OpenCVUtils.init(this)
+        Log.i("Garanzia", "OpenCV initialized=$loaded")
+    }
+}
