c19ba050ba1e33f1122be7ece6432c27db09fb8e
milanstevic1992ts-gif <milanstevic1992ts@gmail.com>
2026-09-26 17:04:54 +0200

    feat(search): bump Room schema to v2 with FTS index

--- app/src/main/java/com/milanstevic/garanzia/data/local/GaranziaDatabase.kt
+++ app/src/main/java/com/milanstevic/garanzia/data/local/GaranziaDatabase.kt
@@ -3,13 +3,14 @@ package com.milanstevic.garanzia.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-const val GARANZIA_SCHEMA_VERSION = 1
+const val GARANZIA_SCHEMA_VERSION = 2
 
 @Database(
     entities = [
         ReceiptEntity::class,
         ReceiptProductEntity::class,
         ReceiptPageEntity::class,
+        ReceiptSearchEntity::class,
     ],
     version = GARANZIA_SCHEMA_VERSION,
     exportSchema = true,
