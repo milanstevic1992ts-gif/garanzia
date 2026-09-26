c04daf6a8bd8598e599fa6d8454787312f1987d7
milanstevic1992ts-gif <milanstevic1992ts@gmail.com>
2026-09-26 18:42:09 +0200

    build(data): add official Room Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("com.google.devtools.ksp") version "2.3.11" apply false
     id("com.google.dagger.hilt.android") version "2.60.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+    id("androidx.room") version "2.8.5" apply false
 }
