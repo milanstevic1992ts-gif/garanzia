1f5424fa7b690eba7e28c56e05d4334991da3700
milanstevic1992ts-gif <milanstevic1992ts@gmail.com>
2026-09-26 09:57:07 +0200

    build(ui): add premium motion and icon support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("com.google.dagger:hilt-android:2.60.1")
