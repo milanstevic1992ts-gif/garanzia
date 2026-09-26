3ca0563938c6dec322de8e3b5fcb1bbb92ccf9a3
milanstevic1992ts-gif <milanstevic1992ts@gmail.com>
2026-09-26 16:58:54 +0200

    feat(data): support transactional receipt edits

--- app/src/main/java/com/milanstevic/garanzia/data/local/ReceiptDao.kt
+++ app/src/main/java/com/milanstevic/garanzia/data/local/ReceiptDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -19,6 +20,13 @@ abstract class ReceiptDao {
     @Insert(onConflict = OnConflictStrategy.ABORT)
     protected abstract suspend fun insertPages(entities: List<ReceiptPageEntity>)
 
+    @Update
+    protected abstract suspend fun updateReceipt(entity: ReceiptEntity)
+
+    @Query("DELETE FROM receipt_products WHERE receiptId = :receiptId")
+    protected abstract suspend fun deleteProductsForReceipt(receiptId: String)
+
+
     @Transaction
     open suspend fun insertReceiptGraph(
         receipt: ReceiptEntity,
@@ -29,6 +37,17 @@ abstract class ReceiptDao {
         insertProducts(products)
         insertPages(pages)
     }
+
+    @Transaction
+    open suspend fun updateReceiptGraph(
+        receipt: ReceiptEntity,
+        products: List<ReceiptProductEntity>,
+    ) {
+        updateReceipt(receipt)
+        deleteProductsForReceipt(receipt.id)
+        insertProducts(products)
+    }
+
 
     @Transaction
     @Query("SELECT * FROM receipts WHERE id = :receiptId LIMIT 1")
